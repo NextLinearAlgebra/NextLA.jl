af019923de69e520501fcad0a98f9602726f8f85
Rabab53 <rabab.omairy@kaust.edu.sa>
2024-08-27 07:18:33 -0400

    Rabab/lu (#3)
    * lu in julia with all pivoting strategy
    
    * Project.toml
    
    * Update Project.toml
    
    * Update DLAMatrix.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,14 @@
 using NextLA
 using Test
 using LinearAlgebra, Random
+using CUDA
 
+#using Aqua
+#@testset "Project quality" begin
+#    Aqua.test_all(NextLA, ambiguities=false)
+#end
+
+include("NextLAMatrix.jl")
+include("lu.jl")
+include("unified_rectrxm.jl")
+include("trsm.jl")
