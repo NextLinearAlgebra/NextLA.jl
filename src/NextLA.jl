c3ef8b8cc961304e8e9aa47eda644291029c31b6
Felipe Tome <tomefelipe0@usp.br>
2025-05-26 15:51:18 -0300

    Minor duplicate cleanup

--- src/NextLA.jl
+++ src/NextLA.jl
@@ -33,56 +33,36 @@ else DEV == :CPU
     Backend = CPU()
 end
 
-"""
-    lamch(::Type{T}, cmach) where{T<: Number}
-
-Determines single / double precision machine parameters
-
-# Arguments
-- T : type, currently only tested Float32 and Float64
-- 'cmach' : specifies the value to be returned by lamch
-    - = 'E': returns eps
-    - = 'S': returns sfmin
-    - = 'P': returns eps*base
-    
-    - where
-        - eps = relative machine precision
-        - sfmin = safe min, such that 1/sfmin does not overflow
-        - base = base of the machine
-"""
-function lamch(::Type{T}, cmach) where{T<: Number}
-    ep = eps(T) 
-    one = oneunit(T)
-    rnd = one
-
-    if one == rnd
-        ep *= 0.5
-    end
-
-    if cmach == 'E'
-        return ep
-    elseif cmach == 'S'
-        sfmin = floatmin(T)
-        small = one / floatmax(T)
-
-        if small >= sfmin
-            sfmin = small*(one + ep)
-        end
-        return sfmin
-    else # assume cmach = 'P'
-        # assume base of machine is 2
-        return ep*2
-    end
-end
-
-# Write your package code here.
 include("NextLAMatrix.jl")
-include("lu.jl")
-include("trmm.jl")
-include("trsm.jl")
-include("rectrxm.jl")
-include("matmul.jl")
-include("zlauu2.jl") 
-include("zlauum.jl")
+include("axpy.jl")   
+include("lauum.jl")         
+include("rectrxm.jl") 
+include("zlarfb_v0.jl")   
+include("zlarfg.jl")  
+include("zpemv.jl")   
+include("zunmqr_v0.jl")
+include("gerc.jl")    
+include("lu.jl")            
+include("trmm.jl")     
+include("zlarfb_v1.jl")   
+include("zlarf.jl")   
+include("ztsmqr.jl") 
+include("zunmqrwrap.jl")
+include("getc2.jl")   
+include("matmul.jl")        
+include("trsm.jl")     
+include("zlarfb_v2.jl")   
+include("zlarft.jl")  
+include("ztsqrt.jl")
+include("getrf2.jl")  
+include("zgeqr2.jl")   
+include("zlarfb_v3.jl")   
+include("zpamm.jl")   
+include("zttmqr.jl")
+include("lauu2.jl")   
+include("zgeqrt.jl")   
+include("zlarfbwrap.jl")  
+include("zparfb.jl")  
+include("zttqrt.jl")
 
 end
--- src/getc2.jl
+++ src/getc2.jl
@@ -156,43 +156,6 @@ function getc2!(A::AbstractMatrix{T}, ipiv::AbstractVector{Int}, jpiv::AbstractV
     return
 end
 
-"""
-    geru!(alpha::T, x::AbstractVector{T}, y::AbstractVector{T}, A::AbstractMatrix{T})
-
-Performs operation A = alpha * x * y^T + A, 
-where alpha is a scalar, x is an m element vector, y is an n element vector, and A is an m-by-n matrix
-"""
-function geru!(alpha::T, x::AbstractVector{T}, y::AbstractVector{T}, A::AbstractMatrix{T}) where T
-    m,n = size(A)
-    # assume incy = incx = 1
-
-    if m < 0
-        return 1
-    end 
-
-    if n < 0
-        return 2
-    end
-
-    if m == 0 || n == 0 || alpha == zero(T)
-        return
-    end
-
-    jy = 1
-
-    for j in 1:n
-        if y[jy] != zero(T)
-            temp = alpha * y[jy]
-            for i in 1:m
-                A[i,j] += x[i] * temp
-            end
-        end
-
-        jy += 1
-    end
-
-    return 
-end
 
 function blas_geru!(alpha::T, x::AbstractVector{T}, y::AbstractVector{T}, A::AbstractMatrix{T}) where T
     m,n = size(A)
