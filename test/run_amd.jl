3e5c885e12a096f9fa5254cbd38b77e0aa398e7c
Evelyne Ringoot <eringoot@mit.edu>
2025-09-20 17:12:56 -0700

    adding amd test file

---
+++ test/run_amd.jl
@@ -0,0 +1,5 @@
+using Test,  LinearAlgebra, Printf, Plots, KernelAbstractions, AMDGPU, CUDA, GPUArrays, GPUArraysCore, oneAPI
+ include("../src/cholesky_tree_subdiv.jl")
+const backend=KernelAbstractions.get_backend(AMDGPU.zeros(2))
+include("cholesky_acc.jl")
+include("cholesky_time.jl")
