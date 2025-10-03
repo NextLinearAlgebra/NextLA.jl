04a169f259ed9c4cfda29be4dc5753fc32e5d0eb
Vicki <vickicar@mit.edu>
2025-10-03 13:51:14 -0400

    adaptive cholesky progress

--- test/chol_nested_tests.jl
+++ test/chol_nested_tests.jl
@@ -11,28 +11,35 @@ flops_potrf(T_prec, n) = (1/3 * n^3 + 1/2 * n^2)
 
 calculate_gflops(flops, time_ns) = (flops / time_ns)
 
+function benchmark_op(op, reset_op, backend)
+    reset_op()
+    op()
+    KernelAbstractions.synchronize(backend)
+
+    min_time_ns = Inf
+    for _ in 1:5
+        reset_op()
+        time = run_single_benchmark(op, backend)
+        min_time_ns = min(min_time_ns, time)
+    end
+    
+    return min_time_ns
+end
 
 function run_cholesky_test(A_spd_fp64::CuMatrix, n::Int, T_prec::DataType, factorization_func!)
-    # Create a clean version of the matrix with the target precision
     A_clean = T_prec.(A_spd_fp64)
-    # Create the matrix to be used for performance measurement
     A_perf = copy(A_clean)
     backend = KernelAbstractions.get_backend(A_perf)
 
-    # Define the operation to be benchmarked, including resetting the matrix each time
-    op_with_reset = () -> begin
-        copyto!(A_perf, A_clean)
-        factorization_func!(A_perf)
-    end
+    op = () -> factorization_func!(A_perf)
+    reset_op = () -> copyto!(A_perf, A_clean)
     
-    # run_manual_benchmark is expected to return the minimum time in nanoseconds
-    min_time_ns = run_manual_benchmark(op_with_reset, backend; min_time_s=1.0, min_iters=5)
+    min_time_ns = benchmark_op(op, reset_op, backend)
     
     runtime_ms = min_time_ns / 1_000_000
     flops = flops_potrf(T_prec, n)
     gflops = calculate_gflops(flops, min_time_ns)
     
-    # Free up GPU memory explicitly
     A_clean = nothing
     A_perf = nothing
     GC.gc(true); CUDA.reclaim()
@@ -43,12 +50,11 @@ end
 
 function run_all_benchmarks()
     n_values = [4096, 8192, 16384, 32768, 65536]
-    precisions = [Float16, Float32, Float64] # Added Float16
+    precisions = [Float32, Float64] # Added Float16
     block_size = 256 
 
     println("="^80)
     println("🚀 Starting Cholesky Factorization Benchmark...")
-    println("   (Accuracy tests have been removed to conserve memory)")
     println("="^80)
 
     for n in n_values
