b246f8314c1f5f6d4137f8745646cdb2d652f870
Vicki <vickicar@mit.edu>
2026-02-04 22:58:07 -0500

    left looking cholesky

--- src/potrf.jl
+++ src/potrf.jl
@@ -301,7 +301,7 @@ function cholesky_lower!(A)
         # grabbing the diagonal block to solve with our custom kernel
         A_diag = view(A, k:k_end, k:k_end)
         
-        kernel = chol_kernel_register!(backend, REG_THREADS)
+        kernel = chol_kernel_lower!(backend, REG_THREADS)
         # crucial: we must force workgroupsize to be 768 so all threads are in the same block
         kernel(A_diag, Val(blk_len); ndrange=REG_THREADS, workgroupsize=REG_THREADS)
         
