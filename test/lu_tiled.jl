e93ad6597b86c946610339cdbee78280181b1363
Vicki <vickicar@mit.edu>
2026-06-03 09:57:33 -0400

    Add 2048 and 4096 to lu_tiled accuracy sizes

--- test/lu_tiled.jl
+++ test/lu_tiled.jl
@@ -8,7 +8,7 @@ using KernelAbstractions
 include("benchmark.jl")
 
 @testset "Accuracy Test for Tiled LU Kernel" begin
-    n_sizes = [32, 64, 128, 256]
+    n_sizes = [32, 64, 128, 256, 512, 1024, 2048, 4096]
     tile_size = 16
     tolerance = 1e-10
 
