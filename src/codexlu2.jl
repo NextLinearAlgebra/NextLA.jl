4521221e9b77a6d53f1764982a57783fb7d31009
Vicki <vickicar@mit.edu>
2026-07-20 13:44:20 -0400

    codex evaluation

--- src/codexlu2.jl
+++ src/codexlu2.jl
@@ -2,6 +2,10 @@ using LinearAlgebra
 using CUDA
 
 abstract type AbstractMixedPrec{T} <: AbstractMatrix{T} end
+
+struct TransposedMixedPrec{T, M <: AbstractMixedPrec{T}} <: AbstractMixedPrec{T}
+    parent::M
+end
  
 include("wrappers.jl")
 include("rectrxm.jl")   # unified_rectrxm!
